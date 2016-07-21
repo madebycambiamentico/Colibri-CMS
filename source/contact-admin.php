39da805c19c3950403681a56cee03eaa0f14c0db
costacurta nereo <costacurta.nereo@gmail.com>
2016-07-21 17:03:53 +0200

    missing namespace in sessionmanager

--- source/contact-admin.php
+++ source/contact-admin.php
@@ -13,7 +13,7 @@ require_once __DIR__ . "/config.php";
 require_once $CONFIG['database']['dir']."functions.inc.php";
 
 //control login
-$SessionManager = new SessionManager();
+$SessionManager = new \Colibri\SessionManager();
 $SessionManager->sessionStart('colibri');
 
 if (!isset(
